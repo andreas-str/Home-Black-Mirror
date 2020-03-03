724f231b892ee17db2edcaf8b83a7d45202686f3
Andreas <andreasstratakis@gmail.com>
2020-03-03 21:58:51 +0100

    added new font, made simple clock app

--- main/main.py
+++ main/main.py
@@ -3,6 +3,7 @@ import display
 import services
 #import external
 import time
+import datetime
 import sys
 import pygame
 import pygame.freetype
@@ -13,9 +14,11 @@ def main():
     running = True
     pygame.init()
     screen = pygame.display.set_mode((400,400), pygame.RESIZABLE)
+    clockobject = pygame.time.Clock()
 
     # main fonts
-    main_font = pygame.freetype.Font("fonts/ARCADECLASSIC.TTF", 50)
+    main_font = pygame.freetype.Font("fonts/Gameplay.ttf", 40)
+    main_font_small = pygame.freetype.Font("fonts/Gameplay.ttf", 23)
 
     while running:
         for event in pygame.event.get():
@@ -25,11 +28,27 @@ def main():
                 if event.unicode == 's':
                     running = False
 
-        screen.fill(display.black)
+        #slow down loop to 5fps
+        clockobject.tick(5)
 
-        main_font.render_to(screen, (0, 0), "Hello World!", display.white)
+        #convert time to 12 hour format
+        hour_now = datetime.datetime.now().time().hour
+        minute_now = datetime.datetime.now().time().minute
+        tfhour = time.strptime(str(hour_now) + " : " + str(minute_now), "%H : %M")
+        twhour_now = time.strftime( "%I : %M", tfhour)
+        pm_am_now = time.strftime( "%p", tfhour)
 
+        #convert date to proper date
+        date_now = str(datetime.datetime.now().date().day) + " - " + str(datetime.datetime.now().date().month)+ " - " + str(datetime.datetime.now().date().year)
+
+        #display stuff
+        main_font.render_to(screen, (10, 10), twhour_now, display.white)
+        main_font_small.render_to(screen, (180, 25), pm_am_now, display.white)
+        main_font_small.render_to(screen, (10, 70), date_now, display.white)
+
+        #update display
         pygame.display.flip()
+        #time.sleep(1)
 
     print ("axnenenenene")
     
