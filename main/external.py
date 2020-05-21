0e1bdf1010000ea9b4b14d67639f226689ee0cbf
Andreas <andreasstratakis@gmail.com>
2020-05-21 11:36:58 +0200

    fuk

--- main/external.py
+++ main/external.py
@@ -81,7 +81,7 @@ def get_rf_data():
             final_list.append(temp)
             final_list.append(hum)
             final_list.append(panel_value)
-            new_rx_data = True
+            Ext_ctrl.new_rx_data = True
             return final_list
     except:
         return None
